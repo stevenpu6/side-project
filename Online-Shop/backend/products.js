527bf1cfa0e0f0ce716b146b6a4df9fedea17bdc
Steven Pu <steven.yuchengpu@gmail.com>
2024-03-16 02:04:19 +0800

    OnlineShop

---
+++ Online-Shop/backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "Vulture",
+    desc: "Money, Relationships, Sex",
+    price: 37.8,
+    image:
+      "https://res.cloudinary.com/dfpdfjvpn/image/upload/v1710323201/onlineShop/Vultures_onfdkp.png",
+  },
+  {
+    id: 2,
+    name: "Donda",
+    desc: "Jail & Hurricane",
+    price: 25.6,
+    image:
+      "https://res.cloudinary.com/dfpdfjvpn/image/upload/v1710323201/onlineShop/Donda_oso4sb.png",
+  },
+  {
+    id: 3,
+    name: "Ye",
+    desc: "TURBO GRAFX 16",
+    price: 40.8,
+    image:
+      "https://res.cloudinary.com/dfpdfjvpn/image/upload/v1710323201/onlineShop/Ye_album_cover_nawgs1.jpg",
+  },
+];
+
+module.exports = products;
